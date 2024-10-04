0636b3afd12e5f95b8287db8160d8c2366834287
Selene Manno <selenepuntoemme@gmail.com>
2024-10-04 17:18:35 +0200

    fix: :bug: fix mispelled font name in layout.tsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,5 @@
+'use client'
+
 import "../global.css";
 import { Inter } from "next/font/google";
 import LocalFont from "next/font/local";
@@ -8,33 +10,33 @@ import Hue from "./components/hue";
 const username = process.env.GITHUB_USERNAME || data.githubUsername;
 const displayName = data.displayName || username;
 
-/** @type {import('next').Metadata} */
-export const metadata = {
-  title: {
-    default: [username, "'s portfolio"].join(""),
-    template: "%s | " + data.displayName + "'s portfolio",
-  },
-  description: "GitHub portfolio for " + displayName,
-  robots: {
-    index: true,
-    follow: true,
-    googleBot: {
-      index: true,
-      follow: true,
-      "max-video-preview": -1,
-      "max-image-preview": "large",
-      "max-snippet": -1,
-    },
-  },
-  icons: [
-    {
-      url: "/favicon.ico",
-      rel: "icon",
-      sizes: "any",
-      type: "image/svg+xml",
-    },
-  ],
-};
+// /** @type {import('next').Metadata} */
+// export const metadata = {
+//   title: {
+//     default: [username, "'s portfolio"].join(""),
+//     template: "%s | " + data.displayName + "'s portfolio",
+//   },
+//   description: "GitHub portfolio for " + displayName,
+//   robots: {
+//     index: true,
+//     follow: true,
+//     googleBot: {
+//       index: true,
+//       follow: true,
+//       "max-video-preview": -1,
+//       "max-image-preview": "large",
+//       "max-snippet": -1,
+//     },
+//   },
+//   icons: [
+//     {
+//       url: "/favicon.ico",
+//       rel: "icon",
+//       sizes: "any",
+//       type: "image/svg+xml",
+//     },
+//   ],
+// };
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
